5d6788ccf01ed5e217f6c40532c75d259fef3c27
eesuhn <eason.yihong@gmail.com>
2024-04-03 00:28:57 +0800

    Fixed terrain image

--- app/utils.py
+++ app/utils.py
@@ -9,7 +9,7 @@ def load_img(path, scale=1):
 	img = pygame.transform.scale(
 		img,
 		(img.get_width() * scale, img.get_height() * scale))
-	img.set_colorkey((0, 0, 0))
+	# img.set_colorkey((0, 0, 0))
 	return img
 
 
