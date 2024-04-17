fdfd34feee87dd6c2861882b08bc2166dc42f86d
eesuhn <eason.yihong@gmail.com>
2024-04-18 01:51:16 +0800

    Added _constants.py

---
+++ app/_costants.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+import os
+import psutil
+
+
+FPS = 60
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 640
+CPU_MONITOR_EVENT = pygame.USEREVENT + 1
+RECT_WIDTH = 64
+RECT_HEIGHT = 64
+ANIMATION_DELAY = 3
--- app/entities/_internal.py
+++ app/entities/_internal.py
@@ -1,6 +1,1 @@
 from .._internal import *
-
-
-RECT_WIDTH = 64
-RECT_HEIGHT = 64
-ANIMATION_DELAY = 3
