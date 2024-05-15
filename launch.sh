1a4c03009faba219bbc5631d65d9c55a27f329cf
eesuhn <eason.yihong@gmail.com>
2024-05-16 02:20:32 +0800

    Updated return for launch script

--- launch.sh
+++ launch.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-GREEN='\033[0;32m'
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
 NC=$(tput sgr0)
 
 VENV_DIR="venv-piksel-bros"
 
-launch() {
-	if [ ! -d "$VENV_DIR" ]; then
-		printf "${YELLOW}Building Environment...\n"
-		python3 -m venv $VENV_DIR
+if [ ! -d "$VENV_DIR" ]; then
+	printf "${YELLOW}Building Environment...\n"
+	python3 -m venv $VENV_DIR
 
-		if [ $? -ne 0 ]; then
-			printf "${RED}Error${NC}: Failed to build environment\n"
-			return 1
-		fi
-		source "$VENV_DIR/bin/activate"
-
-		printf "Installing Requirements...${NC}\n"
-		pip install -r requirements.txt
-
-		if [ $? -ne 0 ]; then
-			printf "${RED}Error${NC}: Failed to install requirements\n"
-			return 1
-		fi
-		printf "${GREEN}Environment Built!${NC}\n"
+	if [ $? -ne 0 ]; then
+		printf "${RED}Error${NC}: Failed to build environment\n"
+		return 1
 	fi
-
 	source "$VENV_DIR/bin/activate"
-	return 0
-}
-launch
+
+	printf "Installing Requirements...${NC}\n"
+	pip install -r requirements.txt
+
+	if [ $? -ne 0 ]; then
+		printf "${RED}Error${NC}: Failed to install requirements\n"
+		return 1
+	fi
+	printf "${GREEN}Environment Built!${NC}\n"
+fi
+
+printf "${GREEN}Launching Environment...${NC}\n"
+source "$VENV_DIR/bin/activate"
+return 0
