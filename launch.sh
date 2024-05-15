6b2cd76a71983841b1bdbd585a48593a97e7de8f
eesuhn <eason.yihong@gmail.com>
2024-05-15 11:47:16 +0800

    Updated launch script

--- launch.sh
+++ launch.sh
@@ -6,15 +6,28 @@ NC=$(tput sgr0)
 
 VENV_DIR="venv-piksel-bros"
 
-if [ ! -d "$VENV_DIR" ]; then
-	printf "${YELLOW}Building Environment...\n"
-	python3 -m venv $VENV_DIR
+launch() {
+	if [ ! -d "$VENV_DIR" ]; then
+		printf "${YELLOW}Building Environment...\n"
+		python3 -m venv $VENV_DIR
+
+		if [ $? -ne 0 ]; then
+			printf "${RED}Error${NC}: Failed to build environment\n"
+			return 1
+		fi
+		source "$VENV_DIR/bin/activate"
+
+		printf "Installing Requirements...${NC}\n"
+		pip install -r requirements.txt
+
+		if [ $? -ne 0 ]; then
+			printf "${RED}Error${NC}: Failed to install requirements\n"
+			return 1
+		fi
+		printf "${GREEN}Environment Built!${NC}\n"
+	fi
+
 	source "$VENV_DIR/bin/activate"
-
-	printf "Installing Requirements...${NC}\n"
-	pip install -r requirements.txt
-
-	printf "${GREEN}Environment Built!${NC}\n"
-fi
-
-source "$VENV_DIR/bin/activate"
+	return 0
+}
+launch
